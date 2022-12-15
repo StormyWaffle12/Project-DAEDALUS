144352f996fc7e130d6aee028c06035bda772b80
Storm <Samjpoyner@icloud.com>
2022-12-15 15:48:06 +0000

    change async-prompt to prompt-sync

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
-const fs=require('fs')
-const prompt=require('prompt-async')
-const me=require('./memory/me.json')
+const fs = require('fs')
+const prompt = require('prompt-sync')()
+const me = require('./memory/me.json')
 
 //memory is untested. probably need to test
-const memory=fs.readdir("./memory",(err, files)=>{
-    files.forEach(file =>{
+const memory = fs.readdir("./memory", (err, files) => {
+    files.forEach(file => {
         return file
     })
 })
 
-var daedalus={
-    "update":function(){
+var daedalus = {
+    "update": function () {
         console.clear()
         console.log(me.name)
-        prompt(">> ")
+        var input=prompt(">> ")
     }
 }
+
+setInterval(()=>{daedalus.update()})
